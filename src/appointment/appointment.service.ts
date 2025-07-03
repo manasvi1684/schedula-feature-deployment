037d8632807877b9b59203c870aec72fbc533fd6
Manasvi Gupta <manasvi.gupta1684@gmail.com>
2025-07-03 12:15:19 +0530

    adding logs to appointment.service.ts

--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -69,6 +69,9 @@ export class AppointmentService {
         if (!patient) {
           throw new NotFoundException('Patient profile not found for the logged-in user.');
         }
+        console.log(`Booking for User UUID: ${userUuid}`);
+        console.log(`Associated Patient ID: ${patient.patient_id}`);
+
         
         const doctor = timeSlot.doctor;
 
